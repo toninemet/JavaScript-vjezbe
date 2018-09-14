f7bf7406355c8510e5d4873c4a569adf57d727fb
toninemet <43183332+toninemet@users.noreply.github.com>
2018-09-14 17:20:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// Uzmi nasumičan - random broj od 1 do 10
+var num = Math.ceil(Math.random() * 10);  
+
+var slanac = prompt("pogogdi broj između 1 i 10");
+
+if (slanac == num) {
+
+	alert("bravo ti si genije");
+
+	
+} 
+else
+{
+
+	alert("netočno traženi broj je: " + num);
+
+}	
